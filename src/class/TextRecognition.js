51ce33e00f2045bf0739dfbabcc06277e8619ee4
Marcelo <marcelo.nasccc@gmail.com>
2021-07-05 22:55:28 -0300

    feat: change loop and other details

--- src/class/TextRecognition.js
+++ src/class/TextRecognition.js
@@ -1,6 +1,7 @@
+const BORDER = 40;
 class TextRecognition {
   constructor(image, imageResult, operator) {
-    this.emptySpaceHeight = 120; // the height of space mask (letter height)
+    this.emptySpaceHeight = 80; // the height of space mask (letter height)
     this.emptySpaceWidth = 20; // the width of space mask
     this.spaceMask = (
       new Array(this.emptySpaceHeight))
@@ -17,27 +18,50 @@ class TextRecognition {
   }
 
   // find first of the text
-  _findStartLine() {
+  _findStartLine(inicialPosition) {
     const matrix = this.image.getMatrix();
 
-    for (let currentLine = 1; currentLine < this.image.getHeight() - 1; currentLine++) { // ignore the border
-      for (let currentPixel = 1; currentPixel < this.image.getWidth() - 1; currentPixel++) { // ignore the border
+    for (let currentLine = inicialPosition; currentLine < this.image.getHeight() - BORDER; currentLine++) { // ignore the border
+      for (let currentPixel = BORDER; currentPixel < this.image.getWidth() - BORDER; currentPixel++) { // ignore the border
         if (matrix[currentLine][currentPixel] === 1) { //first black point
           return currentLine;
         }
       }
     }
 
-    return 0; // not have text
+    return this.image.getHeight() - 1; // not have text
+  }
+
+  _findLineHeight(inicialPosition){
+    const matrix = this.image.getMatrix();
+
+    for (let currentLine = inicialPosition; currentLine < this.image.getHeight() - BORDER; currentLine++) { // ignore the border
+      let isBlankLine = true;
+
+      for (let currentPixel = BORDER; currentPixel < this.image.getWidth() - BORDER; currentPixel++) { // ignore the border
+        if (matrix[currentLine][currentPixel] === 1) { //first black point
+          isBlankLine = false;
+          break;
+        }
+      }
+
+      if(isBlankLine){
+        return currentLine;
+      }
+    }
+
+    console.log(`Not find blank line: ${inicialPosition}`);
+    return inicialPosition + this.emptySpaceHeight; // return mask height
   }
 
   // find last line of the text
   _findLastLine() {
     const matrix = this.image.getMatrix();
 
-    for (let currentLine = this.image.getHeight() - 2; currentLine >= 1; currentLine--) { // ignore the border
-      for (let currentPixel = this.image.getWidth() - 2; currentPixel >= 1; currentPixel--) { // ignore the border
+    for (let currentLine = this.image.getHeight() - BORDER; currentLine > BORDER; currentLine--) { // ignore the border
+      for (let currentPixel = this.image.getWidth() - BORDER; currentPixel > BORDER; currentPixel--) { // ignore the border
         if (matrix[currentLine][currentPixel] === 1) { //last black point
+          console.log(`Last line: ${currentLine}`);
           return currentLine;
         }
       }
@@ -52,16 +76,19 @@ class TextRecognition {
     const imageWidth = this.image.getWidth();
 
     let isInWord = false; // the current position cotains a word
-    let startWord = [0, 0]; // position where the current word start
-    let endWord = [0, 0]; // position where the current word end
+    let startWord = { line: 0, column: 0 }; // position where the current word start
+    let endWord = { line: 0, column: 0 }; // position where the current word end
 
-    for (let currentLine = this._findStartLine(); currentLine <= this._findLastLine(); currentLine += this.emptySpaceHeight) {
+    let currentLine = this._findStartLine(BORDER); // ignore border
+    let endLine = this._findLineHeight(currentLine);
+    const lastLine = this._findLastLine();
+    while (currentLine < lastLine) {
       this.linesCount++;
 
-      let currentPixel = 1; // ignore the border
-      while (currentPixel < this.image.getWidth() - 1) { // ignore the border
+      let currentPixel = BORDER; // ignore the border
+      while (currentPixel < this.image.getWidth() - BORDER) { // ignore the border
         if (isInWord) {
-          const stillInWord = this.operator.erode(
+          const stillInWord = !this.operator.erode(
             {
               matrix,
               startHeight: currentLine,
@@ -78,7 +105,7 @@ class TextRecognition {
 
           if (!stillInWord) { // find end of the word
             isInWord = false;
-            endWord = [currentLine, currentPixel];;
+            endWord = { line: currentLine, column: currentPixel };
             currentPixel += this.emptySpaceWidth;
 
             this.imageResult.setMatrix(
@@ -93,7 +120,7 @@ class TextRecognition {
             currentPixel++;
           }
         } else {
-          const inWord = this.operator.erode(
+          const inWord = !this.operator.erode(
             {
               matrix,
               startHeight: currentLine,
@@ -110,7 +137,7 @@ class TextRecognition {
 
           if (inWord) { // find a black point then start a word
             isInWord = true;
-            startWord = [currentLine, currentPixel];
+            startWord = { line: currentLine, column: currentPixel };
             currentPixel++;
             this.wordsCount++;
           } else { // not find a black point then add mask width
@@ -118,6 +145,13 @@ class TextRecognition {
           }
         }
       }
+
+      console.log(`Start line: ${currentLine} || End Line: ${endLine}`);
+      currentLine = this._findStartLine(endLine);
+      if(currentLine > lastLine) { // already verify last line
+        break;
+      }
+      endLine = this._findLineHeight(currentLine);
     }
   }
 }
