fdc938ddfbdb206a5d0f4254de0a283a649c278a
Marcelo <marcelo.nasccc@gmail.com>
2021-07-05 22:56:00 -0300

    fix: call median filter two times

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ const Operator = require('./class/Operator');
 const TextRecognition = require('./class/TextRecognition');
 
 (async function () {
+  console.log(`start at ${new Date()}`);
   const image = new Image();
   const imageResult = new Image();
-  await image.init(join(__dirname, 'assets', 'grupo_10_imagem_3_linhas_33_palavras_497.pbm'));
-  await imageResult.init(join(__dirname, 'assets', 'grupo_10_imagem_3_linhas_33_palavras_497.pbm'));
+  await image.init(join(__dirname, 'assets', 'grupo_10_imagem_1_linhas_29_palavras_419.pbm'));
+  await imageResult.init(join(__dirname, 'assets', 'grupo_10_imagem_1_linhas_29_palavras_419.pbm'));
 
   const operator = new Operator();
 
+  image.setMatrix(
+    operator.medianFilter(
+      image.getMatrix(),
+      image.getHeight(),
+      image.getWidth()
+    )
+  );
   image.setMatrix(
     operator.medianFilter(
       image.getMatrix(),
@@ -27,5 +35,10 @@ const TextRecognition = require('./class/TextRecognition');
   console.log(`Linhas: ${textRecognition.linesCount}`);
   // image.setMatrix(operator.negative(image.getMatrix(), 1));
 
+  // imageResult.setMatrix(
+  //   operator.negative(imageResult.getMatrix(), 1),
+  // );
+
   await imageResult.saveImage(join(__dirname, 'assets', 'image4Result.pbm'));
+  console.log(`end at ${new Date()}`);
 })();
